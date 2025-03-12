7c4836dfd4b5e14a728536c530a792cc16c25071
LavannyaCSE <lavannyasunil98@gmail.com>
2025-03-12 14:47:38 +0530

    This is my pen

---
+++ Method/Method2.java
@@ -0,0 +1,10 @@
+public class Method2{
+    public void add(int numOne,int numTwo){
+        int numThree = numOne+numTwo;
+        System.out.println(numThree);
+    }
+public static void main(String args[]){
+    Method2 obj =new Method2();
+    obj.add(10,2);
+    }
+    }    
