e7ff56c6402860f080ab8b7542b25aa8542a5335
LavannyaCSE <lavannyasunil98@gmail.com>
2025-03-13 15:09:35 +0530

    Age finf

---
+++ AgeFind/Age.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Age{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter your Age");
+        int Age=input.nextInt();
+        System.out.println(Age);
+
+    }
+}
