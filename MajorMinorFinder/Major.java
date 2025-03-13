02520de230ebf2237a7a3e5704d4b3383df21ca0
LavannyaCSE <lavannyasunil98@gmail.com>
2025-03-13 11:08:40 +0530

    I am Lav

---
+++ MajorMinorFinder/Major.java
@@ -0,0 +1,11 @@
+public class Major{
+    public static void main(String args[]){
+        int age=25;
+        if(age>=18){
+            System.out.println("Major");
+        }
+        else{
+            System.out.println("Minor");
+        }
+    }
+}
