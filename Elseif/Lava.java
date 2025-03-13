c0e46706416eb8d5b76c1c6dd9d00039a36ed2e2
LavannyaCSE <lavannyasunil98@gmail.com>
2025-03-13 13:29:42 +0530

    I am Lavannya

---
+++ Elseif/Lava.java
@@ -0,0 +1,16 @@
+public class Lava{
+    public static void main(String args[]){
+        int num=27;
+        if(num>0){
+            System.out.println("Positive");
+        }
+        else if(num==0){
+
+            System.out.println("Neutral");
+            }
+            else{
+            System.out.println("Negative");
+    
+        }
+    }
+}
