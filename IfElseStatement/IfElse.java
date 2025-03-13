5fd9e9da40f041cecdbfd407cca71187419fd7ff
LavannyaCSE <lavannyasunil98@gmail.com>
2025-03-13 11:00:08 +0530

    It is my book

---
+++ IfElseStatement/IfElse.java
@@ -0,0 +1,11 @@
+public class IfElse{
+    public static void main(String args[]){
+        if(true){
+
+    System.out.println("Correct");
+        }
+    else{
+        System.out.println("Wrong");
+    }
+}
+}
