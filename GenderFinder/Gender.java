39afe0804f7be1a0fc8fa4e119455bd3038acefb
LavannyaCSE <lavannyasunil98@gmail.com>
2025-03-13 11:41:48 +0530

    I am Lavannya

---
+++ GenderFinder/Gender.java
@@ -0,0 +1,13 @@
+public class Gender{
+    public static void main(String args[]){
+        char gender='M';
+        if(gender=='M')
+        {
+            System.out.println("Male");
+        }
+        else{
+            System.out.println("Female");
+        }
+    }
+
+}
