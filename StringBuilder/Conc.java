48ca8d8e4c0e369d31dd6d28a79f03580aeef220
LavannyaCSE <lavannyasunil98@gmail.com>
2025-03-13 12:02:04 +0530

    I have abook

---
+++ StringBuilder/Conc.java
@@ -0,0 +1,8 @@
+public class Conc{
+    public static void main(String args[])
+    {
+        String name1="hello";
+        String name2="world";
+        System.out.println(name1+name2);
+    }
+}
