84ecb59bb917b8bafe6c80c23cf9b46fdb54f4ba
LavannyaCSE <lavannyasunil98@gmail.com>
2025-03-13 10:52:20 +0530

    I love java

---
+++ ConditionalStatement/Cond.java
@@ -0,0 +1,11 @@
+public class Cond{
+    public static void main(String args[]){
+        if(true){
+
+    System.out.println("Correct");
+        }
+    if(false){
+        System.out.println("Wrong");
+    }
+}
+}
