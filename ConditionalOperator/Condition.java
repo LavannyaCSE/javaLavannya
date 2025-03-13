5061000c78f8ce1286b7c7f1c3531510adb41abf
LavannyaCSE <lavannyasunil98@gmail.com>
2025-03-13 10:35:55 +0530

    I am Lavannya

---
+++ ConditionalOperator/Condition.java
@@ -0,0 +1,10 @@
+public class Condition{
+    public static void main(String args[]){
+        System.out.println(61<45);
+        System.out.println(65>12);
+        System.out.println(25>=15);
+        System.out.println(23<=10);
+        System.out.println(60==60);
+        System.out.println(45!=90);
+    }
+}
