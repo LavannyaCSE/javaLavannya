f2921677fdf6b1cebc48ff9b05b5595b13ca08af
LavannyaCSE <lavannyasunil98@gmail.com>
2025-03-13 14:37:04 +0530

    Leap year

---
+++ Leapyear/Leap.java
@@ -0,0 +1,11 @@
+public class Leap{
+    public static void main(String args[]){
+        int year=2020;
+        if(year%4==0){
+            System.out.println("Leap year");
+        }
+        else{
+            System.out.println("Not a leap year");
+        }
+    }
+}
