ffa833c2132ff5505972a62f19d9c44620e4073c
LavannyaCSE <lavannyasunil98@gmail.com>
2025-03-14 11:22:04 +0530

    Vowel Finder

---
+++ VowelFinder/Vow.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Vow
+{
+    public static void main(String args[])
+    {
+        System.out.println("Enter the letter");
+        String v[]={"a","e","i","o","u"};
+        Scanner input = new Scanner(System.in);
+        String letter=input.nextLine();
+        int detect=0;
+        int i=0;
+        while(i<=4){
+        if(letter.equals(v[i]))
+        {
+    
+                detect=1;
+                 }i++;
+        }
+            if(detect==1){
+                System.out.println("vowels");
+            }
+        
+        else
+        {
+            System.out.println("not a vowels");
+        }
+            }
+}
