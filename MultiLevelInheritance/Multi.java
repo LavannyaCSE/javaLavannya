79ae7778c5d4aac4cdf9120218ce3e511293909e
LavannyaCSE <lavannyasunil98@gmail.com>
2025-03-14 13:52:46 +0530

    MultiLevel Inheritance

---
+++ MultiLevelInheritance/Multi.java
@@ -0,0 +1,23 @@
+class Parent{
+    public void add(){
+        System.out.println("hello world");
+    }
+}
+class Child extends Parent{
+    void display(){
+        add();
+    }
+}
+class Third extends Child{
+    void helper(){
+        display();
+    }
+    
+
+}
+public class Multi{
+    public static void main(String arg[]){
+    Third obj =new Third();
+    obj.helper();
+}
+}
