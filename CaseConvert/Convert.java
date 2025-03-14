3e141df47bcd67d2ca3a9817b2ebc95236681e44
LavannyaCSE <lavannyasunil98@gmail.com>
2025-03-14 12:10:27 +0530

    Case converter

---
+++ CaseConvert/Convert.java
@@ -0,0 +1,9 @@
+public class Convert
+{
+    public static void main(String args[])
+    {
+        String name="Lavannya";
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+    }
+}
