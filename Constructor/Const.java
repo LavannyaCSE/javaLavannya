f70a716e5077439d95326c7b1d833b6b9bbbc143
LavannyaCSE <lavannyasunil98@gmail.com>
2025-03-15 10:50:31 +0530

    Constructor

---
+++ Constructor/Const.java
@@ -0,0 +1,11 @@
+public class Const
+{
+    public static void main(String args[]){
+        Const obj = new Const();
+        obj.Const();
+        }
+        public void Const()
+        {
+            System.out.println("Hello");
+        }
+}
